a753dcbc4ba9aef7bff296cfbee5fb7a5541d6ab
Darlin Ruben Nina C <chinausto@gmail.com>
2023-03-06 11:40:53 -0400

    routers_delete_login

--- src/Routers/routers.tsx
+++ src/Routers/routers.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Admin } from "../page/admin/admin";
 import { AgregarUsuario } from "../page/AgregarUsuario/AgregarUsuario";
-import { Login } from "../auth/login/Login";
 import { VerUsuarios } from "../page/VerUsuarios/VerUsuarios";
 function Routers() {
   return (
@@ -10,7 +9,6 @@ function Routers() {
         <Route path="/" element={<Admin />} />
         <Route path="/AgregarUsuario" element={<AgregarUsuario />} />
         <Route path="/VerUsuarios" element={<VerUsuarios />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
